121dc812fec5c2c0b59a59a5f5c81deaa249a271
Michele Kempinsky <mkempinsky@icloud.com>
2018-06-12 19:42:11 -0700

    playing with charts

--- scripts.js
+++ scripts.js
@@ -3,14 +3,28 @@ var chart = new Chart(ctx, {
     // The type of chart we want to create
     type: 'line',
 
+
+
     // The data for our dataset
     data: {
-        labels: ["January", "February", "March", "April", "May", "June", "July"],
+        labels: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
         datasets: [{
             label: "My First dataset",
-            backgroundColor: 'rgb(255, 99, 132)',
-            borderColor: 'rgb(255, 99, 132)',
-            data: [0, 10, 5, 2, 20, 30, 45],
+            backgroundColor: 'rgba(148,231,190, .5)',
+            borderColor: 'rgb(148,231,190)',
+            borderWidth: 3,
+            pointBackgroundColor: '#fff',
+            pointRadius: 5,
+            data: [0, 10, 5, 2, 20, 30, 45, 30, 23, 17, 22, 40],
+        },
+        {
+            label: "My Second dataset",
+            backgroundColor: 'rgba(231,148,189, .5)',
+            borderColor: 'rgb(231,148,189)',
+            borderWidth: 3,
+            pointBackgroundColor: '#fff',
+            pointRadius: 5,
+            data: [7, 13, 5, 4, 15, 23, 30, 27, 21, 33, 40, 45],
         }]
     },
 
